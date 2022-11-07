819b1254a1617c6c1230b2b9637bb651451024be
dvphuc96 <phuc.dovan.96@gmail.com>
2022-11-07 18:33:39 +0700

    update validate

--- util/Validation.js
+++ util/Validation.js
@@ -9,34 +9,42 @@ var validation = {
     return true;
   },
   checkPhone: (value, errId, name) => {
-    var regexNumberPhone = new RegExp(/^[0-9]+$/);
-    if (!regexNumberPhone.test(value)) {
-      document.getElementById(errId).style.display = "block";
-      document.getElementById(errId).innerHTML = `${name} phải là số!`;
-      return false;
+    if (value.trim() !== "") {
+      var regexNumberPhone = new RegExp(/^[0-9]+$/);
+      if (!regexNumberPhone.test(value)) {
+        document.getElementById(errId).style.display = "block";
+        document.getElementById(errId).innerHTML = `${name} phải là số!`;
+        return false;
+      }
+      document.getElementById(errId).style.display = "none";
+      return true;
     }
-    document.getElementById(errId).style.display = "none";
-    return true;
   },
   checkEmail: (value, errId, name) => {
-    var regexEmail = new RegExp(
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\ [[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-    if (!regexEmail.test(value)) {
-      document.getElementById(errId).style.display = "block";
-      document.getElementById(errId).innerHTML = `${name} không hợp lệ!`;
-      return false;
+    if (value.trim() !== "") {
+      var regexEmail = new RegExp(
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\ [[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+      if (!regexEmail.test(value)) {
+        document.getElementById(errId).style.display = "block";
+        document.getElementById(errId).innerHTML = `${name} không hợp lệ!`;
+        return false;
+      }
+      document.getElementById(errId).style.display = "none";
+      return true;
     }
-    document.getElementById(errId).style.display = "none";
-    return true;
   },
   checkMatch: (password, passwordConfirm, errId) => {
-    if (password !== passwordConfirm) {
-      document.getElementById(errId).style.display = "block";
-      document.getElementById(errId).innerHTML = `Password confirm phải giống với password!`;
-      return false;
+    if (password.trim() !== "" && passwordConfirm.trim() !== "") {
+      if (password !== passwordConfirm) {
+        document.getElementById(errId).style.display = "block";
+        document.getElementById(
+          errId
+        ).innerHTML = `Password confirm phải giống với password!`;
+        return false;
+      }
+      document.getElementById(errId).style.display = "none";
+      return true;
     }
-    document.getElementById(errId).style.display = "none";
-    return true;
   },
 };
